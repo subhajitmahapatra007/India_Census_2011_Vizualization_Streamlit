688c293190d335185a11ac039fb6215354be9f56
subhajitmahapatra007 <subhajitmahapatra71992@gmail.com>
2026-09-04 22:03:15 +0530

    file error handle

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("india_census_2011.csv")
+    df = pd.read_csv("India_census_2011.csv")
 
     # Create useful calculated metrics
     df["Internet Penetration (%)"] = (
